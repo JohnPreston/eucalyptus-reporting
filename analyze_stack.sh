1f21f5e5bca2d3588e299c554e6eac174ae5fe57
John Mille <john@ews-network.net>
2016-06-15 18:53:16 +0000

    Date of stack creation of the stack

--- analyze_stack.sh
+++ analyze_stack.sh
@@ -3,15 +3,18 @@
 ACCOUNT=$1
 DATE=`date +%Y%m%d-%H%M-%Z`
 
-for STACK_NAME in `euform-describe-stacks --region "$ACCOUNT"@ | grep STA | awk '{print $2}'` ; do
-    echo $STACK_NAME >> $DATE.csv
+for STACK_LINE in `euform-describe-stacks --region "$ACCOUNT"@ | grep STA | awk '{print $2","$NF}'` ; do
+    echo "Stack Name,Creation date" >> $DATE.csv
+    STACK_NAME=`echo -n $STACK_LINE | awk -F\, '{print $1}'`
+    echo STACK NAME IS $STACK_NAME
+    echo $STACK_LINE >> $DATE.csv
     echo "Instances" >> $DATE.csv
     echo "quantity,instance-type" >> $DATE.csv
     euca-describe-instances --region aimia@ | grep TAG | grep environment_name | grep $STACK_NAME | awk '{print $3}' | xargs -i -P10 euca-describe-instances {} --region aimia@ | \
-	grep INST | grep running | awk '{print $9}' | sort | uniq -c | awk '{print $1","$2}' >> $DATE.csv
+    	grep INST | grep running | awk '{print $9}' | sort | uniq -c | awk '{print $1","$2}' >> $DATE.csv
 
     echo "Volumes" >> $DATE.csv
     echo "quantity,size(GB)" >> $DATE.csv
     euca-describe-volumes --region aimia@ | grep TAG | grep stack-name | grep $STACK_NAME | awk '{print $3}' | xargs -i -P1 euca-describe-volumes --region aimia@ {} | grep VOL | awk '{print $3}' | sort | uniq -c | awk '{print $1","$2}' >> $DATE.csv
-    echo
+    echo >> $DATE.csv
 done
